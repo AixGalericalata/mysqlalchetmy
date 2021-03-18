8905d30b6ed25a2735893705804f8af274e5b8ef
Nesya <angerasimov04@outlook.com>
2021-03-18 16:12:30 +0300

    main changed

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template
 from werkzeug.utils import redirect
 
@@ -176,7 +178,8 @@ def fill_db():
 def main():
     db_session.global_init("db/blogs.db")
     #  fill_db()
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
